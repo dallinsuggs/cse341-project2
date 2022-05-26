33cda3152b656022052f1ba8333a8f8f30331166
Dallin Suggs <dallin.suggs@gmail.com>
2022-05-26 07:26:35 -0600

    add error handling to requests

--- routes/bucketlist.js
+++ routes/bucketlist.js
@@ -9,61 +9,100 @@ routes.use(express.json());
 
 //GET ALL BUCKETLIST ITEMS
 routes.get('/', (req, res) => {
-  const results = connect.getCollection().find({});
-  results.toArray().then((documents) => {
-    res.status(200).json(documents);
-    console.log('Returned ALL bucket listings')
-  });
+  try {
+    const results = connect.getCollection().find({});
+    results.toArray()
+      .then((documents) => {
+        res.status(200).json(documents);
+        console.log('Returned ALL bucket listings')
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Some error occurred with accessing the database.'
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //GET 1 ITEM
 routes.get('/:id', (req, res) => {
-  const itemId = new ObjectId(req.params.id);
-  const results = connect.getCollection().find({ _id: itemId });
-  results.toArray().then((documents) => {
-    res.status(200).json(documents[0]);
-    console.log(`Returned bucketlist item ${req.params.id}`);
-  });
+  try {
+    const itemId = new ObjectId(req.params.id);
+    const results = connect.getCollection().find({ _id: itemId });
+    results.toArray()
+      .then((documents) => {
+        res.status(200).json(documents[0]);
+        console.log(`Returned bucketlist item ${req.params.id}`);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Some error occurred with accessing the database.'
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //POST 1 NEW BUCKETLIST ITEM
 routes.post('/', (req, res) => {
-  console.log(req.body);
-
-  const newDoc = new Object({
-    name: req.body.name,
-    deadline: req.body.deadline,
-    priority: req.body.priority
-  });
-  const result = connect.getCollection().insertOne(newDoc)
-    .then(result => {
-      res.json(`New listing created with the following id: ${result.insertedId}`);
-    })
-    .catch(error => console.error(error));
-  console.log(`New listing created with the following id: ${result.insertedId}`);
+  try{
+    const newDoc = new Object({
+      name: req.body.name,
+      deadline: req.body.deadline,
+      priority: req.body.priority
+    });
+    const result = connect.getCollection().insertOne(newDoc)
+      .then(result => {
+        res.json(`New listing created with the following id: ${result.insertedId}`);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Some error occurred while creating a new listing.'
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //PUT UPDATE TO CONTACT
 routes.put('/:id', (req, res) => {
-  const itemId = new ObjectId(req.params.id);
-  const newDoc = new Object(req.body);
-  connect.getCollection().replaceOne({ _id: itemId }, newDoc)
-    .then(result => {
-      res.json(`Updated listing for id ${req.params.id}`);
-    })
-    .catch(error => console.error(error));
-  console.log(`Updated listing for id ${req.params.id}`);
+  try {
+    const itemId = new ObjectId(req.params.id);
+    const newDoc = new Object(req.body);
+    connect.getCollection().replaceOne({ _id: itemId }, newDoc)
+      .then(result => {
+        res.json(`Updated listing for id ${req.params.id}`);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Some error occurred while updating the listing.'
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //DELETE CONTACT
 routes.delete('/:id', (req, res) => {
-  const itemId = new ObjectId(req.params.id);
-  connect.getCollection().deleteOne({ _id: itemId })
-    .then(result => {
-      res.json(`Deleted listing id ${req.params.id}`);
-    })
-    .catch(error => console.error(error));
-  console.log(`Deleted listing id ${req.params.id}`);
+  try {
+    const itemId = new ObjectId(req.params.id);
+    connect.getCollection().deleteOne({ _id: itemId })
+      .then(result => {
+        res.json(`Deleted listing id ${req.params.id}`);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Some error occurred while deleting the listing.'
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = routes;
