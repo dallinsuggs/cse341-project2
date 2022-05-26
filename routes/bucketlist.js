8b4595b3949889c2e09c4795c37b9e7aa1607d75
Dallin Suggs <dallin.suggs@gmail.com>
2022-05-26 06:47:49 -0600

    add put and delete

--- routes/bucketlist.js
+++ routes/bucketlist.js
@@ -43,5 +43,27 @@ routes.post('/', (req, res) => {
   console.log(`New listing created with the following id: ${result.insertedId}`);
 });
 
+//PUT UPDATE TO CONTACT
+routes.put('/:id', (req, res) => {
+  const itemId = new ObjectId(req.params.id);
+  const newDoc = new Object(req.body);
+  connect.getCollection().replaceOne({ _id: itemId }, newDoc)
+    .then(result => {
+      res.json(`Updated listing for id ${req.params.id}`);
+    })
+    .catch(error => console.error(error));
+  console.log(`Updated listing for id ${req.params.id}`);
+});
+
+//DELETE CONTACT
+routes.delete('/:id', (req, res) => {
+  const itemId = new ObjectId(req.params.id);
+  connect.getCollection().deleteOne({ _id: itemId })
+    .then(result => {
+      res.json(`Deleted listing id ${req.params.id}`);
+    })
+    .catch(error => console.error(error));
+  console.log(`Deleted listing id ${req.params.id}`);
+});
 
 module.exports = routes;
