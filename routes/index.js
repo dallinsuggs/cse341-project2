f4934b93660d28b72c90ca0a21adc479b8a73940
Dallin Suggs <dallin.suggs@gmail.com>
2022-06-09 11:42:11 -0600

    error

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,10 @@ routes.get('/', ensureGuest, (req, res) => {
 // @desc Api-docs
 // @route GET /api-docs
 routes.get('/api-docs', ensureAuth, async (req, res) => {
-  res.render('api-docs', {
-    name: req.user.firstName
-  })
+  res.render('Req.isAuthenticated: ' + req.isAuthenticated());
+  // res.render('api-docs', {
+  //   name: req.user.firstName
+  // })
 })
 
 routes.use('/auth', require('./auth'));
